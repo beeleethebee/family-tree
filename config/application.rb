d953c0422d0b051c69789797e9be0bd481b039fd
Huugoo147 <hugo.vast@gmail.com>
2020-07-12 11:23:59 +0200

    React routers OK

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,10 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 Dotenv::Railtie.load
-module FamilyTree
+
+module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
